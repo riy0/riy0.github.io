eb438bd3bd381f3ec244172fc3a3fbaa42c2476d
riy0 <chac0ptx@gmail.com>
2020-01-08 18:39:58 +0900

    Updates

---
+++ precache-manifest.86ccf4b55e5217deafc6d30a52f9537f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8051b903dd412d634b5559b78d80a781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de79f9934ef679c5e948",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "1bf21c76d794456ebed2",
+    "url": "/static/css/main.682de690.chunk.css"
+  },
+  {
+    "revision": "de79f9934ef679c5e948",
+    "url": "/static/js/2.79034735.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.79034735.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bf21c76d794456ebed2",
+    "url": "/static/js/main.353352c3.chunk.js"
+  },
+  {
+    "revision": "a5fcb6e6ae83f2c25723",
+    "url": "/static/js/runtime-main.f39b43cb.js"
+  }
+]);
