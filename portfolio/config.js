d8e5e0f1176d901bbc0eccdec5a114ccb6d960d5
riy0 <chac0ptx@gmail.com>
2020-01-19 20:27:50 +0900

    update/ use gatsby

---
+++ portfolio/config.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Portfolio by Ryosuke",
+  subtitle: "I am Ryosuke. Web developer and TED Translator",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 5,
+  googleAnalyticsId: "UA-73379983-2",
+  useKatex: false,
+  menu: [
+    {
+      label: "Works & Talks",
+      path: "/"
+    },
+    {
+      label: "About me",
+      path: "/pages/about"
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts"
+    }
+  ],
+  author: {
+    name: "Ryosuke Mishima",
+    photo: "/photo.jpg",
+    bio: "I am Ryosuke. Web developer and TED Translator",
+    contacts: {
+      email: "chac0ptx@gmail.com",
+      facebook: "ryosuke.mishima.75",
+      telegram: "#",
+      twitter: "#",
+      github: "riy0",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "honoporo",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: ""
+    }
+  }
+};
---
+++ portfolio/content/pages/contacts.md
@@ -0,0 +1,11 @@
+---
+title: "Contact me"
+template: "page"
+socialImage: "/media/image-4.jpg"
+---
+
+Resume: https://resume.io/r/S5t6IVnHj
+Github: https://github.com/riy0
+TED: https://www.ted.com/profiles/5448352/translator
+Instagram: https://www.instagram.com/honoporo/
+
